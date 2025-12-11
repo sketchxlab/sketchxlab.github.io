859f1ec8ce9e0bc54a5c5879df35d368cd97538f
BossMac <bossmac@BossMacs-MacBook-Air.local>
2025-12-11 17:27:58 +0000

    Revert "use html as main file"
    This reverts commit 98e0c08c0d06d75afc28ce8602a5b67f56bd0cd4.

---
+++ app.js
@@ -0,0 +1,2 @@
+// This file is deprecated. The application logic has moved to main.js.
+// You can delete this file.
---
+++ index.tsx
@@ -0,0 +1,2 @@
+// This file is deprecated. The application logic has moved to main.js to resolve server loading issues.
+// You can delete this file.
